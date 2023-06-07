43d8f0fd00a4932b46129dcd3a09fcdc45a7ced6
MEDEXA\björn paulsen <bjorn.paulsen@medexa.se>
2023-06-07 10:51:06 +0200

    finally made it work

--- Annoyotron/MainWindow.xaml.cs
+++ Annoyotron/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using Annoyotron.ViewModels;
 
 namespace Annoyotron
 {
@@ -10,46 +11,9 @@ namespace Annoyotron
     /// </summary>
     public partial class MainWindow : Window
     {
-        // This should go into MainWindowViewModel.cs
-        public ObservableCollection<ViewItem> Views { get; } =
-            ViewItemFactory.CreateViewItems;
-
-        // This should be kept in MainWindowViewModel.cs, as it is the state we wish to alter.
-        private ViewItem _selectedView;
-
-        // This should be handled differently, in some purer type of XAML-based binding deal.
-        // Is that even a thing?
-        public ViewItem SelectedView
-        {
-            get => _selectedView;
-            set
-            {
-                if (_selectedView == value)
-                {
-                    return;
-                }
-                _selectedView = value;
-                ContentControl.Content = _selectedView.View;
-                OnPropertyChanged();
-            }
-        }
-
-        // See above: if SelectedView is gone, we cut this
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // See above: if SelectedView is gone, we cut this, and good fecking riddance.
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
         public MainWindow()
         {
             InitializeComponent();
-
-            // This should all be handled in XAML.
-            AuthenticationMethodSelector.SelectedIndex = 0;
-            SelectedView = Views[0]; // Set the initial selected view
         }
     }
 }
---
+++ Annoyotron/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace Annoyotron.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+
+    {
+        public static ObservableCollection<ViewItem> Views { get; } =
+            ViewItemFactory.CreateViewItems;
+
+        // This should be kept in MainWindowViewModel.cs, as it is the actual state we want to toggle.
+        private ViewItem _selectedView = Views.FirstOrDefault();
+
+        // This should be handled differently, in some purer type of XAML-based binding deal.
+        // Is that even a thing?
+        public ViewItem SelectedView
+        {
+            get => _selectedView;
+            set
+            {
+                if (_selectedView == value)
+                {
+                    return;
+                }
+
+                SetField(ref _selectedView, value);
+                _selectedView = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
