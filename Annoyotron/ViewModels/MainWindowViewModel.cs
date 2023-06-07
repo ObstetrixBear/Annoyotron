068c9ef6ef95bc8909d5401b2e957057863eeecf
MEDEXA\björn paulsen <bjorn.paulsen@medexa.se>
2023-06-07 10:58:20 +0200

    cleanup

--- Annoyotron/ViewModels/MainWindowViewModel.cs
+++ Annoyotron/ViewModels/MainWindowViewModel.cs
@@ -3,43 +3,30 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Windows.Controls;
 
 namespace Annoyotron.ViewModels
 {
+    // This shows no references because it's referred to  in the XAML rather than the code.
     public class MainWindowViewModel : INotifyPropertyChanged
-
     {
         public static ObservableCollection<ViewItem> Views { get; } =
             ViewItemFactory.CreateViewItems;
 
-        // This should be kept in MainWindowViewModel.cs, as it is the actual state we want to toggle.
         private ViewItem _selectedView = Views.FirstOrDefault();
 
-        // This should be handled differently, in some purer type of XAML-based binding deal.
-        // Is that even a thing?
         public ViewItem SelectedView
         {
             get => _selectedView;
-            set
-            {
-                if (_selectedView == value)
-                {
-                    return;
-                }
-
-                SetField(ref _selectedView, value);
-                _selectedView = value;
-            }
+            set => SetField(ref _selectedView, value);
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) 
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
+        // Basically encapsulates the "if value is unchanged do nothing, else
+        // set value and post PropertyChanged message" dealio.
         protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
         {
             if (EqualityComparer<T>.Default.Equals(field, value)) return false;
